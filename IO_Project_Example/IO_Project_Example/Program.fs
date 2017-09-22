007d27f8fcaa11bbedf21ab367b8ebaf872268d0
Thomas Nix <tnix2@uncc.edu>
2017-09-22 13:16:07 -0400

    Started I/O Program, reads from console and outputs text.

---
+++ IO_Project_Example/IO_Project_Example/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+
+// Example of reading from and writing to the console
+let getUserInput() = 
+
+  // Ask user for name
+  printf("Please enter your name: ");
+
+  // Read in users name
+  let usersName = Console.ReadLine()
+
+  // Print the users name to the console 
+  printfn "Greetings, %s" usersName
+
+
+// Function call 
+getUserInput()
+
+// Keeps console open until user hits a key
+Console.ReadKey() |> ignore
