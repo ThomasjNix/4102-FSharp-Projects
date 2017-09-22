00d82cb2dcb81551403cfcae935b0b94a2367fd7
Thomas Nix <tnix2@uncc.edu>
2017-09-22 13:40:58 -0400

    Now reads from test file in debug path, prints to the console. Moved console keystroke ignore statement to it's own function.

--- IO_Project_Example/IO_Project_Example/Program.fs
+++ IO_Project_Example/IO_Project_Example/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System
+open System.IO
 
 
 // Example of reading from and writing to the console
@@ -10,12 +11,31 @@ let getUserInput() =
   // Read in users name
   let usersName = Console.ReadLine()
 
-  // Print the users name to the console 
-  printfn "Greetings, %s" usersName
+  // Print the users name to the console
+  printfn "Greetings, %s\n" usersName
 
+// Example of reading from a file and printing to the console
+let readFromFile(inString : string) = 
+ 
+  // Print string sent from function call
+  printfn "%s" inString
 
-// Function call 
+  // Read all lines from file
+  let file = "Testfile.txt"
+  let lineArray = File.ReadAllLines(file)
+
+  // Loop through line array and print line
+  for i = 0 to lineArray.Length - 1 do
+    printfn "%s" lineArray.[i]
+
+// Keeps console open until user hits a key  
+let keepConsoleOpen() = 
+  printfn "\nPress any key to exit"
+  Console.ReadKey() |> ignore
+
+// Function calls
 getUserInput()
+readFromFile("A haiku for your worries:")
+keepConsoleOpen()
 
-// Keeps console open until user hits a key
-Console.ReadKey() |> ignore
+
